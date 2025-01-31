09e0dbf9d4e0c835a1ab451b04ccaaea8e99a327
deborasarah <160731012+deborasarah@users.noreply.github.com>
2025-01-31 10:54:14 -0300

    Add files via upload

--- home/views.py
+++ home/views.py
@@ -1,4 +1,3 @@
-# home/views.py
 from django.shortcuts import render
 
 def index(request):
@@ -12,3 +11,25 @@ def sobre(request):
 
 def ajuda(request):
     return render(request, 'ajuda.html')
+
+def local(request):
+    return render(request, 'local.html')
+    
+def exibiritem(request, id):
+    return render(request, 'exibiritem.html', {'id': id})
+
+def perfil(request, usuario):
+    return render(request, 'perfil.html', {'usuario': usuario})
+
+def dia_da_semana(request, num):
+    dias = {
+        1: "Domingo",
+        2: "Segunda-feira",
+        3: "Terça-feira",
+        4: "Quarta-feira",
+        5: "Quinta-feira",
+        6: "Sexta-feira",
+        7: "Sábado"
+    }
+    dia = dias.get(num, "Número inválido! Escolha um número entre 1 e 7.")
+    return render(request, 'dia.html', {'dia': dia})
--- piloto-main/home/urls.py
+++ piloto-main/home/urls.py
@@ -6,8 +6,4 @@ urlpatterns = [
     path('contato/', views.contato, name='contato'),  # Página de contato
     path('sobre/', views.sobre, name='sobre'),
     path('ajuda/', views.ajuda, name='ajuda'),
-    path('local/', views.local, name='local'),
-    path('exibiritem/<int:id>/', views.exibiritem, name='exibiritem'),
-    path('perfil/<str:usuario>/', views.perfil, name='perfil'),
-    path('dia/<int:num>/', views.dia_da_semana, name='dia_da_semana'),
 ]
